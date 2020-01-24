fbd9bed88c846af97c8a89623ee6cc39acd8d409
Adam <adamcpenman@gmail.com>
2020-01-24 16:11:26 -0600

    added a delete, put, and post route

--- resources/resources-router.js
+++ resources/resources-router.js
@@ -22,4 +22,29 @@ router.post("/", async (req, res, next) => {
   }
 })
 
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  ReRo.remove(id)
+    .then(deleted => {
+      if(deleted) {
+        res.json({ removed: deleted })
+      } else {
+        res.status(404).json({ message: "could not find users"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to delete"})
+    })
+})
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const resource = await ReRo.update(req.params.id, req.body)
+      res.status(200).json(resource)
+  } catch (err) {
+      next(err)
+  }
+})
+
 module.exports = router;
