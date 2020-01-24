d9b32b8bbdffb84a0fac143ffe5e8d1074aef7a0
Adam <adamcpenman@gmail.com>
2020-01-23 18:22:44 -0600

    resources-model

--- resources/resources-model.js
+++ resources/resources-model.js
@@ -10,7 +10,31 @@ function findById(id) {
     .first()
 }
 
+async function insert(resource) {
+    const [id] = await db("resources")
+        .insert(resource)
+        return findById(id)
+}
+
+async function update(id, changes) {
+    await db("resources")
+        .where({ id })
+        .update(changes)
+
+        return findById(id)
+}
+
+function remove(id) {
+    return db("resources")
+    .where({ id })
+    .del()
+}
+
+
 module.exports = {
     list,
     findById,
+    insert,
+    update,
+    remove,
 }
